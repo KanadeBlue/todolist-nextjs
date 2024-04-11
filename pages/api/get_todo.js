d41179e19e57e8aaf66adcb1830fe01f7a901c91
KanadeBlue <124839201+KanadeBlue@users.noreply.github.com>
2024-04-11 22:27:41 +0000

    ffffffffffffff

--- pages/api/get_todo.js
+++ pages/api/get_todo.js
@@ -6,12 +6,13 @@ import Cookies from 'cookies'
 export default async function handler(req, res) {
   const session = await getServerSession(req, res, authOptions)
   if (req.method == 'GET') {
-    const todos = await get_user_without_set("KanadeBlue")
-    console.log(todos.todo)
+    const oldData = await get_user_without_set("KanadeBlue")
+    var todos = oldData.todo
+    console.log(todos)
     if (typeof todos == "string"){
-        res.status(200).json({ success: false, msg: todos.todo, data: null });
+        res.status(200).json({ success: false, msg: todos, data: null });
     } else {
-        res.status(200).json({ success: true, msg: null, data: todos.todo });
+        res.status(200).json({ success: true, msg: null, data: todos });
     }
   } else {
     res.status(200).json({ success: false, msg: "Send a Get request", data: null });
